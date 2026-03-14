2c45fdfdae23d6130e791d7c1f09deb7b05c91b0
Ishwari Nemade <ishwarinemade26@gmail.com>
2026-03-14 12:30:47 +0530

    Test-01

---
+++ Test_01/src/Test01/Test.java
@@ -0,0 +1,52 @@
+package Test01;
+
+public class Test {
+	public static void main(String[] args) {
+
+        int n = 5;
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+
+            // spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // stars and spaces inside
+            for (int j = 1; j <= (2 * i - 1); j++) {
+
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+
+            // spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // stars and spaces
+            for (int j = 1; j <= (2 * i - 1); j++) {
+
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+
+            System.out.println();
+        }
+    }
+}
